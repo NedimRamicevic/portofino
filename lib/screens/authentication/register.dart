9e23ad8e7bc26275766b9fe85652fbd8d9c36672
Nedim <mr.ramicevic@gmail.com>
2022-01-30 16:53:42 +0300

    registerRestrictions

--- lib/screens/authentication/register.dart
+++ lib/screens/authentication/register.dart
@@ -10,6 +10,7 @@ class Register extends StatefulWidget {
 
 class _RegisterState extends State<Register> {
   final AuthService _auth = AuthService();
+  final GlobalKey<FormState> _registerKey = GlobalKey<FormState>();
   String email = "";
   String password = "";
   @override
@@ -24,42 +25,49 @@ class _RegisterState extends State<Register> {
         padding: EdgeInsets.symmetric(vertical: 20, horizontal: 50),
         color: Colors.amberAccent[400],
         child: Form(
+            key: _registerKey,
             child: Column(
-          children: <Widget>[
-            SizedBox(
-              height: 20,
-            ),
-            TextFormField(
-              onChanged: (val) async {
-                setState(() {
-                  email = val;
-                });
-              },
-            ),
-            SizedBox(
-              height: 20,
-            ),
-            TextFormField(
-              obscureText: true,
-              onChanged: (val) async {
-                setState(() {
-                  password = val;
-                });
-              },
-            ),
-            SizedBox(
-              height: 20,
-            ),
-            ElevatedButton(
-              style: ElevatedButton.styleFrom(primary: Colors.purple),
-              child: Text("Register"),
-              onPressed: () async {
-                print(email);
-                print(password);
-              },
-            )
-          ],
-        )),
+              children: <Widget>[
+                SizedBox(
+                  height: 20,
+                ),
+                TextFormField(
+                  validator: (value) =>
+                      value!.isEmpty ? "write an email" : null,
+                  onChanged: (val) async {
+                    setState(() {
+                      email = val;
+                    });
+                  },
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                TextFormField(
+                  validator: (value) =>
+                      value!.length < 6 ? "at least 6 characters" : null,
+                  obscureText: true,
+                  onChanged: (val) async {
+                    setState(() {
+                      password = val;
+                    });
+                  },
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                ElevatedButton(
+                  style: ElevatedButton.styleFrom(primary: Colors.purple),
+                  child: Text("Register"),
+                  onPressed: () async {
+                    if (_registerKey.currentState!.validate()) {
+                      print(email);
+                      print(password);
+                    }
+                  },
+                )
+              ],
+            )),
       ),
     );
   }
