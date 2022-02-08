028815bba871c3b7c124ecaa2d12ce56a8bcab88
Nedim <mr.ramicevic@gmail.com>
2022-02-08 10:55:01 +0300

    createBrewListWidget

--- lib/screens/home/home.dart
+++ lib/screens/home/home.dart
@@ -1,6 +1,8 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:portofino/services/auth.dart';
+import 'package:portofino/services/database.dart';
 import 'package:provider/provider.dart';
 
 class Home extends StatelessWidget {
@@ -10,21 +12,26 @@ class Home extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final brews = Provider.of<QuerySnapshot>(context);
-    return Scaffold(
-      backgroundColor: Colors.amberAccent,
-      appBar: AppBar(
-        backgroundColor: Colors.amber,
-        elevation: 0,
-        title: const Text("Welcome Home"),
-        actions: [
-          ElevatedButton.icon(
-              onPressed: () async {
-                await _auth.signOut();
-              },
-              icon: const Icon(Icons.person),
-              label: const Text("logout"))
-        ],
+    final user = Provider.of<User>(context);
+    return StreamProvider.value(
+      value: DataBaseService(uid: user.uid).brews,
+      initialData: null,
+      child: Scaffold(
+        backgroundColor: Colors.amberAccent,
+        appBar: AppBar(
+          backgroundColor: Colors.amber,
+          elevation: 0,
+          title: const Text("Welcome Home"),
+          actions: [
+            ElevatedButton.icon(
+                onPressed: () async {
+                  await _auth.signOut();
+                },
+                icon: const Icon(Icons.person),
+                label: const Text("logout"))
+          ],
+        ),
+        body: ,
       ),
     );
   }
